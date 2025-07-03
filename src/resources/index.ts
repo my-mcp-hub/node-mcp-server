71b3bcc0e7addc8abf7c621e8ade105718a92aec
sunzx <sunzx@qcc.com>
2025-07-03 10:52:42 +0800

    feat: add search resource

---
+++ src/resources/index.ts
@@ -0,0 +1,26 @@
+import { McpServer, ResourceTemplate } from '@modelcontextprotocol/sdk/server/mcp.js'
+import type { OptionsType } from '../types'
+
+export const registerResources = (server: McpServer, options: OptionsType) => {
+  // @ts-ignore
+  server.registerResource(
+    'search',
+    new ResourceTemplate('search://{keyword}', {
+      list: undefined,
+    }),
+    {
+      title: 'Search Resource',
+      description: 'Dynamic generate search resource',
+    },
+    async (uri, { keyword }) => {
+      return {
+        contents: [
+          {
+            uri: uri.href,
+            text: `hello ${keyword}`,
+          },
+        ],
+      }
+    },
+  )
+}
