684af314440791f47da6fafec68a511b7a00ccb8
zhensherlock <zhensherlock@126.com>
2025-08-18 09:43:50 +0800

    test: add path alias for src directory in vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   test: {
     globalSetup: ['./vitest.global.ts'],
     setupFiles: ['./vitest.setup.ts'],
